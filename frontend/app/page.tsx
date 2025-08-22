c141eb9dfade7d8be797dd2eccfacafe80b5834f
nehiyrgroup <support@nehiyr-tech.com>
2025-08-22 14:57:12 +0200

    :lipstick: Mise en confirmité de la page connexion au design

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      Bienvenue sur Finance4All
+    <div className="font-sans bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <div className="text-black">
+        Bienvenue sur Finance4All
+
+        <p className="mt-10">Connexion</p>
+      </div>
     </div>
   );
 }
