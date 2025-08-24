3fda0f46ff8ce9ef1f83ef84592f00aa1d990dd8
Nehiyr-Tech <support@nehiyr-tech.com>
2025-08-24 22:00:34 +0200

    :rocket: Mise en place restriction qualité de code Sonarqube

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <div className="navbar-header">
+          Test container navbar
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar;
