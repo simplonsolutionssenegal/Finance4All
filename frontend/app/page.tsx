4531cffc703ba498725c51086df948a06e968e45
nehiyrgroup <support@nehiyr-tech.com>
2025-08-25 18:59:13 +0200

    :lipstick: (wip) Intégration template

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,8 @@ export default function Home() {
     <div className="font-sans bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <div className="text-black">
         Bienvenue sur Finance4All
-        <p className="mt-10">Connexion</p>
+
+        Intégration version site
       </div>
     </div>
   );
