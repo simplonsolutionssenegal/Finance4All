79286c8a25d8148aa471d5101360ad2075a01e45
nehiyrgroup <support@nehiyr-tech.com>
2025-08-27 20:28:15 +0200

    :white_check_mark: Mise à jour des tests unitaires

--- frontend/components/public/layout/footer.tsx
+++ frontend/components/public/layout/footer.tsx
@@ -1,7 +1,4 @@
-
-
-const PublicFooter = () => {
-
+function PublicFooter() {
   return (
     <footer className="bg-gray-900 text-white py-16">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
--- frontend/components/public/layout/header.tsx
+++ frontend/components/public/layout/header.tsx
@@ -1,6 +1,4 @@
-
-const PublicHeader = () => {
-
+function PublicHeader() {
   return <header className="bg-white shadow-sm">
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex justify-between items-center h-16">
