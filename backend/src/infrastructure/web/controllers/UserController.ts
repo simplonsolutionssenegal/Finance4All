b7dbf6b0da6e4f18defeb550b408d4fc0e777164
Diilaye <issa.k@saharux.com>
2025-08-29 15:52:17 +0000

    fix architecture hexagonal minimale structure

--- backend/src/infrastructure/web/controllers/UserController.ts
+++ backend/src/infrastructure/web/controllers/UserController.ts
@@ -1,12 +1,10 @@
 import { Request, Response } from 'express';
-import { CreateUserUseCase } from '../../../application/use-cases/CreateUserUseCase';
 
 export class UserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
+  constructor() {}
 
   async create(req: Request, res: Response) {
     const { name, email } = req.body;
-    const user = await this.createUserUseCase.execute(name, email);
-    res.status(201).json(user);
+    res.status(201).json({});
   }
 }
--- backend/src/infrastructure/web/routes/user.routes.ts
+++ backend/src/infrastructure/web/routes/user.routes.ts
@@ -1,12 +1,9 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/UserController';
 import { PrismaUserRepository } from '../../database/PrismaUserRepository';
-import { CreateUserUseCase } from '../../../application/use-cases/CreateUserUseCase';
 
 const router = Router();
-const userRepository = new PrismaUserRepository();
-const createUserUseCase = new CreateUserUseCase(userRepository);
-const userController = new UserController(createUserUseCase);
+const userController = new UserController();
 
 router.post('/', (req, res) => userController.create(req, res));
 
