38e1a2b89ea0b3c9027caa843d894bb229220b60
nehiyrgroup <support@nehiyr-tech.com>
2025-10-06 15:58:36 +0200

    :sparkles: FIN-49 - Mise en place de l'infrastructure

--- backend/src/application/shared/Mapper.ts
+++
@@ -1,4 +0,0 @@
-export interface Mapper<Domain, DTO> {
-  toDomain(dto: DTO): Domain;
-  toDTO(domain: Domain): DTO;
-}
--- backend/src/domain/institutions/repositories/InstitutionRepository.ts
+++
@@ -1,6 +0,0 @@
-import type { Institution } from '@/domain/institutions/entities/Institution';
-
-export interface InstitutionRepository {
-  save(institution: Institution): Promise<Institution>;
-  findByName(name: string): Promise<Institution[]>;
-}
