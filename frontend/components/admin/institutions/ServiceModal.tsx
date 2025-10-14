b60d34048ccd19e5627d031aaa4f2d9076f11043
Cheikhou NIASSE <niassecheikhou@gmail.com>
2025-10-14 17:51:49 +0000

    :fire: (visualisation produit):supprimer le service repository

--- frontend/components/admin/institutions/ServiceModal.tsx
+++ frontend/components/admin/institutions/ServiceModal.tsx
@@ -54,7 +54,7 @@ const serviceSchema = z.object({
       TypeService.ASSURANCE,
       TypeService.AUTRES,
     ],
-    'Le type de service doit être valide'
+    { errorMap: () => ({ message: 'Le type de service doit être valide' }) }
   ),
   frais: z.object({
     montantFixe: z.number().optional(),
