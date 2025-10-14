cfc810a24d75a97db6b59b975ef9bf26244fde3d
moustaphafayeext <mfaye.ext@simplon.co>
2025-10-14 12:33:15 +0000

    :arrow_down: 33 élimination du code non valide

--- backend/src/infrastructure/config/migrations/20251010132902_init/migration.sql
+++ backend/src/infrastructure/config/migrations/20251010132902_init/migration.sql
@@ -1,12 +1,6 @@
 -- CreateEnum
 CREATE TYPE "public"."InstitutionStatus" AS ENUM ('ACTIVE', 'INACTIVE', 'PENDING');
 
--- CreateEnum
-CREATE TYPE "public"."ProductType" AS ENUM ('CREDIT', 'EPARGNE', 'MOBILE_MONEY', 'INVESTISSEMENT', 'ASSURANCE');
-
--- CreateEnum
-CREATE TYPE "public"."RemboursementMode" AS ENUM ('AGENCE', 'USSD', 'MOBILE');
-
 -- CreateTable
 CREATE TABLE "public"."User" (
     "id" TEXT NOT NULL,
@@ -31,30 +25,8 @@ CREATE TABLE "public"."Institution" (
     CONSTRAINT "Institution_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "public"."Product" (
-    "id" TEXT NOT NULL,
-    "designation" TEXT NOT NULL,
-    "montantMin" DECIMAL(18,2) NOT NULL,
-    "montantMax" DECIMAL(18,2) NOT NULL,
-    "type" "public"."ProductType" NOT NULL,
-    "modesRemboursement" "public"."RemboursementMode" NOT NULL,
-    "institutionId" TEXT NOT NULL,
-    "zones" TEXT[],
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
-);
-
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Institution_name_key" ON "public"."Institution"("name");
-
--- CreateIndex
-CREATE INDEX "Product_institutionId_idx" ON "public"."Product"("institutionId");
-
--- AddForeignKey
-ALTER TABLE "public"."Product" ADD CONSTRAINT "Product_institutionId_fkey" FOREIGN KEY ("institutionId") REFERENCES "public"."Institution"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
