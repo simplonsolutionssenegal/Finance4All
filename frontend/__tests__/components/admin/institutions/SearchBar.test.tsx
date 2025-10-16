ee1a6b52eb06f48fc4b6adbb2f09795fc11b591b
moustaphafayeext <mfaye.ext@simplon.co>
2025-10-16 00:37:28 +0000

    :arrow_down: Filter les services avec afficher des frais

--- frontend/__tests__/components/admin/institutions/SearchBar.test.tsx
+++ frontend/__tests__/components/admin/institutions/SearchBar.test.tsx
@@ -322,7 +322,7 @@ describe('SearchBar', () => {
       const filters: FilterOptions = {
         ...EMPTY_FILTERS,
         type: [TypeService.CREDIT, TypeService.EPARGNE],
-        Coût: ['Gratuit'],
+        Coût: [true],
       };
 
       render(<SearchBar {...defaultProps} currentFilters={filters} />);
