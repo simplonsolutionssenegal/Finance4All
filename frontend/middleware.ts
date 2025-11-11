646c740ad1a3e5cf0fd40cec11cf8d1374a1d737
nehiyrgroup <support@nehiyr-tech.com>
2025-11-11 15:31:13 +0100

    :rocket: Update config

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -15,7 +15,7 @@ export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes, EXCEPT health check
-    '/(api|trpc)(?!/health)(.*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 };
